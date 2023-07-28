3259c705fdd9886f12ca56ec7f4e1fec627edc4c
Jamieho1 <72865004+Jamieho1@users.noreply.github.com>
2023-07-28 16:40:22 +0800

    Add files via upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,13 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 import assets from '../assets'; // Import the entire assets object
 
-// Now access individual assets like this:
-const linkedin = assets.linkedin;
-const github = assets.github;
-const gmail = assets.gmail;
-const phone = assets.phone;
-
 const Contact = () => {
+  // Use individual assets directly without accessing them through `assets`
+  const linkedin = assets.linkedin;
+  const github = assets.github;
+  const gmail = assets.gmail;
+  const phone = assets.phone;
+
   return (
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
